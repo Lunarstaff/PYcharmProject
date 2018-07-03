6fa18b64ea7d09e3d2f578d684224feb2e4f0169
Lunarstaff <lywtesting@163.com>
2018-07-03 22:30:12 +0800

    20180703 找到了读取中文文件的方法，case_generatev2.0.py 开始使用BS4解析.mm文件生成的.html文件。

--- venv/testcode/case_generate/case_generatev2.0.py
+++ venv/testcode/case_generate/case_generatev2.0.py
@@ -13,10 +13,10 @@ case_str = get_case_file_str(case_html_f)
 from bs4 import BeautifulSoup
 case_soup = BeautifulSoup(case_str, "html.parser")
 case_d = {"node": "text"}
-root = case_soup.find(attrs="basetext")
-cs = []
-case_d[case_soup.find(attrs="col").div.text.strip()] = cs
-for i in case_soup.find(attrs="subexp").children:
-    cs.append(i.div.text.strip())
-print(case_d)
-print(cs)
+case_div = case_soup.find(attrs="basetext")
+case_root_text = case_div.div.text
+print(case_root_text.strip())
+print("case_div is " + case_div.name)
+case_div_li = case_div.find(attrs="subexp")
+for i in case_div_li.children:
+    print(i.li.div)
